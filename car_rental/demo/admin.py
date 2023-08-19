287ae2a9f7dcfd3c82bf0aee3cf1a085f144b386
zuhd-dev <study.husniddin@gmail.com>
2023-08-19 18:42:12 +0500

    review models

--- car_rental/demo/admin.py
+++ car_rental/demo/admin.py
@@ -6,8 +6,8 @@ from demo.models import Car
 class CarAdmin(admin.ModelAdmin):
 
     model = Car
-    list_display = ('id', 'model', 'make', 'year', 'color', 'mileage', 'fuel_type', 'price_per_day', 'availability_status')
-    list_filter = ('model','make','year', 'color', 'mileage', 'fuel_type', 'price_per_day', 'availability_status')
+    list_display = ('id', 'model', 'make', 'year', 'color', 'mileage', 'fuel_type', 'price_per_day','avaibilaty_status')
+    list_filter = ('model','make','year', 'color', 'mileage', 'fuel_type', 'price_per_day', )
     search_fields = ('model','make')
     list_display_links = ('id','model')
     list_per_page = 10
--- car_rental/demo/models.py
+++ car_rental/demo/models.py
@@ -5,7 +5,7 @@ class Car(models.Model):
 
     model = models.CharField('модель автомобиля', max_length=100)
     make = models.CharField('марка автомобиля', max_length=100)
-    year = models.EmailField('год выпуска автомобиля')
+    year = models.IntegerField('год выпуска автомобиля')
     color = models.CharField('цвет автомобиля', max_length=50)
     mileage = models.IntegerField('пробег автомобиля')
     fuel_type = models.CharField('тип топлива', max_length=50)
